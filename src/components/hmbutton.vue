614145868cdb76ba8e67d7b40c172b6096f0231f
张亮 <2286377528@qq.com>
2020-01-03 18:13:23 +0800

    03，封装了按钮，并且通过父传子，子传父的方式建立了连接，在login登录页面显示

---
+++ src/components/hmbutton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="btn" @click='handclick' >
+<!-- 字体图标的运用 -->
+<span :class="icon"></span>
+<!-- 匿名插槽的使用 -->
+<slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  // 父传子
+  props: ['icon'],
+  //   子传父
+  methods: {
+    handclick(event) {
+      this.$emit('click', event)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+// 按钮样式
+  .btn {
+     width: 318*100vw/360;
+      height: 50px;
+      color: #fff;
+      font-size: 20px;
+       font-weight: 700;
+      text-align: center;
+      line-height: 50px;
+      border-radius: 25px;
+      background-color: red;
+      margin: 0 auto;
+  }
+</style>
--- src/view/login.vue
+++ src/view/login.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="login"></div>
+  <div class="login">
+
+<hmbutton @click='handerclick'>登录</hmbutton>
+
+  </div>
 </template>
 
 <script>
+// 引入按钮
+import hmbutton from '@/components/hmbutton'
 export default {
-
+  // 注册
+  components: {
+    hmbutton
+  },
+  methods: {
+    handerclick() {
+      console.log(111222333)
+    }
+  }
 }
 </script>
 
