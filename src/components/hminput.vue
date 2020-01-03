9dcb6767578651d11fd6c0f490f047c2076909a2
张亮 <2286377528@qq.com>
2020-01-03 18:36:26 +0800

    04，完成文本框的封装，并且引入登录页面使用，完成基本静态页面

---
+++ src/components/hminput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" class="input">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang= 'less' scoped>
+.input {
+      width: 318*100vw/360;
+      height: 50px;
+      font-size: 20px;
+      line-height: 50px;
+      outline: none;
+      border: none;
+      border-bottom: 3px solid #ccc;
+}
+.red {
+    border-bottom: 3px solid red;
+}
+.green {
+    border-bottom: 3px solid green;
+}
+</style>
--- src/view/login.vue
+++ src/view/login.vue
@@ -1,27 +1,76 @@
 <template>
-  <div class="login">
-
-<hmbutton @click='handerclick'>登录</hmbutton>
-
+  <div id="app">
+    <div class="container">
+      <div class="close">
+        <span class="iconfont"></span>
+      </div>
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="inputs">
+        <!-- 封装的文本框 -->
+        <!-- 用户名框 -->
+        <hminput placeholder="请输入手机号/用户名"></hminput>
+        <!-- 文本框 -->
+        <hminput placeholder="密码"></hminput>
+      </div>
+      <p class="tips">
+        没有账号？
+        <a href="#/register" class>去注册</a>
+      </p>
+      <!-- 封装的按钮 -->
+      <hmbutton @click="handerclick">登录</hmbutton>
+    </div>
   </div>
 </template>
 
 <script>
 // 引入按钮
-import hmbutton from '@/components/hmbutton'
+import hmbutton from '@/components/hmbutton';
+// 引入封装的文本框
+import hminput from '@/components/hminput.vue'
 export default {
   // 注册
   components: {
-    hmbutton
+    hmbutton, hminput
   },
   methods: {
     handerclick() {
-      console.log(111222333)
+      console.log(111222333);
     }
   }
-}
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+// 登录页面的样式
+.container {
+  padding: 20px;
+}
+.close {
+  span {
+    font-size: 27 / 360 * 100vw;
+  }
+}
+.logo {
+  display: flex;
+  justify-content: center;
+  span {
+    display: block;
+    font-size: 126 / 360 * 100vw;
+    color: #d81e06;
+  }
+}
+.inputs {
+  input {
+    margin-bottom: 20px;
+  }
+}
+.tips {
+  text-align: right;
+  margin-bottom: 20px;
+ a {
+    color: #3385ff;
+  }
+}
 </style>
