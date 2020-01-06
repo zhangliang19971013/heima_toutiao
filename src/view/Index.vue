2071507ad79b889cfe0ea6991fb3c03c4fa3a4ab
张亮 <2286377528@qq.com>
2020-01-06 20:46:12 +0800

    18,初步完善首页页面，完成顶部栏的样式增加和基本路由跳转

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -1,13 +1,58 @@
 <template>
-  <div class="index">这是首页</div>
+  <div class="index">
+    <!-- 头部 -->
+   <div class="header">
+     <div class="logo"><span class="iconfont iconnew"></span></div>
+     <div class="search" @click='$router.push({name :"Search"})'>
+       <van-icon name="search" />
+       搜索商品
+     </div>
+     <div class="uesr" @click='$router.push({path : `/personal/${id}`})'>
+       <van-icon name="manager-o" />
+     </div>
+   </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      id: ''
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.header {
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: red;
+  font-size : 25px;
+  color : #fff;
+  .search{
+    flex : 1;
+    height: 40px;
+    font-size: 15px;
+    border-radius: 20px;
+    line-height: 40px;
+    text-align: center;
+    background-color: rgba(255,255,255,.6);
+  }
+  i {
+    padding: 0 10px;
+  }
+  .logo {
+    padding: 0 ,10px;
+      .iconnew {
+      font-size: 60px;
+       }
+    }
+  }
 
 </style>
